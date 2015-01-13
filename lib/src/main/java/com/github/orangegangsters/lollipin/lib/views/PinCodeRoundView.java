b28011440dd707289c72c2599253c49a25db1824
olivierg13 <olivier.goutay@omadahealth.com>
2015-01-13 12:08:17 -0800

    Added PinCodeRoundView (work in progress)

--- lib/src/main/java/com/github/orangegangsters/lollipin/lib/views/PinCodeRoundView.java
+++ lib/src/main/java/com/github/orangegangsters/lollipin/lib/views/PinCodeRoundView.java
@@ -4,26 +4,31 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.github.orangegangsters.lollipin.lib.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by stoyan and olivier on 1/13/15.
+ * Created by stoyan and oliviergoutay on 1/13/15.
  */
-public class KeyboardView extends RelativeLayout {
+public class PinCodeRoundView extends RelativeLayout {
 
     private Context mContext;
+    private List<ImageView> mRoundViews;
 
-    public KeyboardView(Context context) {
+    public PinCodeRoundView(Context context) {
         this(context, null);
     }
 
-    public KeyboardView(Context context, AttributeSet attrs) {
+    public PinCodeRoundView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public KeyboardView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public PinCodeRoundView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
         this.mContext = context;
@@ -36,9 +41,13 @@ public class KeyboardView extends RelativeLayout {
                     defStyleAttr, 0);
 
             LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            KeyboardView view = (KeyboardView) inflater.inflate(R.layout.view_keyboard, this);
+            PinCodeRoundView view = (PinCodeRoundView) inflater.inflate(R.layout.view_round_pin_code, this);
 
-
+            mRoundViews = new ArrayList<>();
+            mRoundViews.add((ImageView) view.findViewById(R.id.pin_code_round1));
+            mRoundViews.add((ImageView) view.findViewById(R.id.pin_code_round2));
+            mRoundViews.add((ImageView) view.findViewById(R.id.pin_code_round3));
+            mRoundViews.add((ImageView) view.findViewById(R.id.pin_code_round4));
         }
     }
 }
