a32724898d08402b2afe4043b7e7327e58c8b3db
callmepeanut <callmepeanut21@gmail.com>
2016-01-14 16:21:55 +0800

    add PinCompatActivity and necessary changes

---
+++ lib/src/main/java/com/github/orangegangsters/lollipin/lib/PinCompatActivity.java
@@ -0,0 +1,77 @@
+package com.github.orangegangsters.lollipin.lib;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.orangegangsters.lollipin.lib.interfaces.LifeCycleInterface;
+import com.github.orangegangsters.lollipin.lib.managers.AppLockActivity;
+
+/**
+ * Created by callmepeanut on 16-1-14.
+ * You must extend this Activity in order to support this library.
+ * Then to enable PinCode blocking, you must call
+ * {@link com.github.orangegangsters.lollipin.lib.managers.LockManager#enableAppLock(android.content.Context, Class)}
+ */
+public class PinCompatActivity extends AppCompatActivity {
+    private static LifeCycleInterface mLifeCycleListener;
+    private final BroadcastReceiver mPinCancelledReceiver;
+
+    public PinCompatActivity() {
+        super();
+        mPinCancelledReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                finish();
+            }
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        IntentFilter filter = new IntentFilter(AppLockActivity.ACTION_CANCEL);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mPinCancelledReceiver, filter);
+    }
+
+    @Override
+    protected void onResume() {
+        if (mLifeCycleListener != null) {
+            mLifeCycleListener.onActivityResumed(PinCompatActivity.this);
+        }
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mLifeCycleListener != null) {
+            mLifeCycleListener.onActivityPaused(PinCompatActivity.this);
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mPinCancelledReceiver);
+    }
+
+    public static void setListener(LifeCycleInterface listener) {
+        if (mLifeCycleListener != null) {
+            mLifeCycleListener = null;
+        }
+        mLifeCycleListener = listener;
+    }
+
+    public static void clearListeners() {
+        mLifeCycleListener = null;
+    }
+
+    public static boolean hasListeners() {
+        return (mLifeCycleListener != null);
+    }
+}
