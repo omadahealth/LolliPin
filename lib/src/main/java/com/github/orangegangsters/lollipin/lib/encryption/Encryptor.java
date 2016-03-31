81cb5cb8f6a61acc86844cafe1986b76903702d6
Huang Yu <huang@bitgo.com>
2016-03-31 15:11:12 -0700

    merge sha1 with sha256

--- lib/src/main/java/com/github/orangegangsters/lollipin/lib/encryption/Encryptor.java
+++ lib/src/main/java/com/github/orangegangsters/lollipin/lib/encryption/Encryptor.java
@@ -31,42 +31,27 @@ public class Encryptor {
 	}
 
 	/**
-	 * Allows to get the SHA1 of a {@link java.lang.String} using {@link java.security.MessageDigest}
+	 * Allows to get the SHA of a {@link java.lang.String} using {@link java.security.MessageDigest}
+	 * if device does not support sha-256, fall back to sha-1 instead
 	 */
-	public static String getSHA1(String text) {
-		String sha1 = null;
+	public static String getSHA(String text) {
+		String sha = null;
 		if (TextUtils.isEmpty(text)) {
-			return sha1;
+			return sha;
 		}
-		MessageDigest sha1Digest = null;
+		MessageDigest shaDigest = null;
 		try {
-			sha1Digest = MessageDigest.getInstance("SHA-1");
+			shaDigest = MessageDigest.getInstance("SHA-256");
 		} catch (Exception e) {
-			return sha1;
+			try {
+				shaDigest = MessageDigest.getInstance("SHA-1");
+			} catch (Exception e) {
+				return sha;
+			}
 		}
 		byte[] textBytes = text.getBytes();
-		sha1Digest.update(textBytes, 0, text.length());
-		byte[] sha1hash = sha1Digest.digest();
-		return bytes2Hex(sha1hash);
-	}
-
-	/**
-	 * Allows to get the SHA256 of a {@link java.lang.String} using {@link java.security.MessageDigest}
-	 */
-	public static String getSHA256(String text) {
-		String sha1 = null;
-		if (TextUtils.isEmpty(text)) {
-			return sha1;
-		}
-		MessageDigest sha1Digest = null;
-		try {
-			sha1Digest = MessageDigest.getInstance("SHA-256");
-		} catch (Exception e) {
-			return sha1;
-		}
-		byte[] textBytes = text.getBytes();
-		sha1Digest.update(textBytes, 0, text.length());
-		byte[] sha1hash = sha1Digest.digest();
-		return bytes2Hex(sha1hash);
+		shaDigest.update(textBytes, 0, text.length());
+		byte[] shahash = shaDigest.digest();
+		return bytes2Hex(shahash);
 	}
 }
