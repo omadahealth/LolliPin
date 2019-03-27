cd1fb886321ae8d0b37ce19cfbe6c18fdecf61cb
Orest.Hredil <Orest.Hredil@medhost.com>
2019-03-27 13:59:35 +0200

    fixed bug with NullPointerException related to FingerPrintManager

--- lib/src/main/java/com/github/orangegangsters/lollipin/lib/managers/AppLockActivity.java
+++ lib/src/main/java/com/github/orangegangsters/lollipin/lib/managers/AppLockActivity.java
@@ -141,7 +141,8 @@ public abstract class AppLockActivity extends PinActivity implements KeyboardBut
             mFingerprintManager = (FingerprintManager) getSystemService(Context.FINGERPRINT_SERVICE);
             mFingerprintUiHelper = new FingerprintUiHelper.FingerprintUiHelperBuilder(mFingerprintManager).build(mFingerprintImageView, mFingerprintTextView, this);
             try {
-                if (mFingerprintManager.isHardwareDetected() && mFingerprintUiHelper.isFingerprintAuthAvailable()
+                if (mFingerprintManager != null && mLockManager.getAppLock() != null
+                        && mFingerprintManager.isHardwareDetected() && mFingerprintUiHelper.isFingerprintAuthAvailable()
                         && mLockManager.getAppLock().isFingerprintAuthEnabled()) {
                     mFingerprintImageView.setVisibility(View.VISIBLE);
                     mFingerprintTextView.setVisibility(View.VISIBLE);
