e468ba0c033d86429cedb85c1a21e1ed893175d1
Ryazantsev Dmitry <dilix90@gmail.com>
2019-04-16 17:47:09 +0200

    Fix crash on rare Nokia devices

--- lib/src/main/java/com/github/omadahealth/lollipin/lib/managers/AppLockActivity.java
+++ lib/src/main/java/com/github/omadahealth/lollipin/lib/managers/AppLockActivity.java
@@ -144,7 +144,7 @@ public abstract class AppLockActivity extends PinActivity implements KeyboardBut
             mFingerprintManager = (FingerprintManager) getSystemService(Context.FINGERPRINT_SERVICE);
             mFingerprintUiHelper = new FingerprintUiHelper.FingerprintUiHelperBuilder(mFingerprintManager).build(mFingerprintImageView, mFingerprintTextView, this);
             try {
-            if (mFingerprintManager.isHardwareDetected() && mFingerprintUiHelper.isFingerprintAuthAvailable()
+            if (mFingerprintManager != null && mFingerprintManager.isHardwareDetected() && mFingerprintUiHelper.isFingerprintAuthAvailable()
                     && mLockManager.getAppLock().isFingerprintAuthEnabled()) {
                     mFingerprintImageView.setVisibility(View.VISIBLE);
                     mFingerprintTextView.setVisibility(View.VISIBLE);
